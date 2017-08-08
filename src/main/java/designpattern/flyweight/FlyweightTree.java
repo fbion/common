8070b75b2043486aa256e07495e4bb5d37a49771
zhihui.zhou <zhihui.zhou@pcitc.com>
2017-08-08 15:38:53 +0800

    common

---
+++ src/main/java/designpattern/flyweight/FlyweightTree.java
@@ -0,0 +1,14 @@
+package designpattern.flyweight;
+
+/**
+ * 描述： <br>
+ * 创建时间: 2017/8/815:27 <br>
+ *
+ * @author 周志辉
+ */
+public class FlyweightTree {
+
+    public static void display(int xCoord, int yCoord, int age) {
+        System.out.println("Position is (" + xCoord + " , " + yCoord + ") , age is " + age + ".");
+    }
+}
