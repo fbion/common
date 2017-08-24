3e635fdf5f73e2b5382d44ff5e00ca962accf0db
zhihui.zhou <zhihui.zhou@pcitc.com>
2017-08-24 14:47:20 +0800

    common

--- src/main/java/test/Test36.java
+++ src/main/java/test/Test36.java
@@ -1,9 +1,12 @@
 package test;
 
-import net.sf.json.JSONArray;
+import com.beust.jcommander.internal.Lists;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * 描述： <br>
@@ -13,28 +16,69 @@ import java.util.Arrays;
  */
 public class Test36 {
 
-    static{
-        if(true) {
-            System.out.println("123");
-            while (true) {
+
+    /**
+     * 字符串里出现三遍及以上的字符去掉
+     */
+    public static void test() {
+        String str = "11adfaasfwqpkwefapadgjqw....$$$^^^[[[";
+        Pattern p = Pattern.compile("(.*)(.)(.*)\\2(.*)\\2(.*)");
+        String temp = str;
+        Matcher m = p.matcher(temp);
+        while (m.find()) {
+            System.out.print(m.group(2) + "\t");
+            temp = temp.replace(m.group(2), "");
+            m = p.matcher(temp);
+        }
+        ;
+        System.out.println(temp);
+
+        //定义一个字符串
+        String string = "11adfaasfwqpkwefapadgjqw....$$$^^^[[[";
+        //遍历字符串的所有字符
+        for (int i = 0; i < string.length(); i++) {
+            //取得第i个字符，也可以用Character.toString(string.charAt(i));
+            String s = string.substring(i, i + 1);
+            //如果通过s在这个字符串中出现了至少3次，输出该字符
+            if (Pattern.matches(".*" + Pattern.quote(s) + ".*" + Pattern.quote(s) + ".*" + Pattern.quote(s) + ".*", string)) {
+                System.out.print(s + "\t");
             }
         }
     }
 
+
+    static String str = "11adfaasfwqpkwefapadgjqw....$$$^^^[[[";
+
     public static void main(String[] args) {
-        ArrayList list1 = new ArrayList();
-        list1.addAll(Arrays.asList("123456".split("")));
+        String test = "11adfaasfwqpkwefapadgjqw....$$$^^^[[[";
+//        List<List<String>> lists =
+//                Arrays.asList(str.split("")).stream().collect(Collectors.groupingBy(n -> n))
+//                        .values().stream()
+//                        .filter(n -> n
+//                                .size() >= 3).collect(Collectors.toList());
+//        System.out.println(lists);
 
-        ArrayList list2 = new ArrayList();
-        list2.addAll(Arrays.asList("789".split("")));
+        Arrays.asList(str.split("")).stream().collect(Collectors.groupingBy(
+                Function.identity(), Collectors.counting()
+        )).entrySet().stream().forEach(n -> {
+            if (n.getValue() >= 3) {
+                str = str.replace(n.getKey(), "");
+            }
+        });
+        System.out.println(str);
 
-        JSONArray jsonArray1 = JSONArray.fromObject(list1);
-        JSONArray jsonArray2 = JSONArray.fromObject(list2);
-        JSONArray jsonArray = new JSONArray();
-        jsonArray.addAll(jsonArray1);
-        jsonArray.addAll(jsonArray2);
-        System.out.println(jsonArray1);
-        System.out.println(jsonArray2);
-        System.out.println(jsonArray);
+        Arrays.asList(test.split(""))
+                .stream()
+                .collect(Collectors.groupingBy(n -> n))
+                .values()
+                .stream()
+                .filter(n -> n.size() > 3)
+                .reduce(Lists.newArrayList(), (prev, next) -> {
+                    prev.addAll(next);
+                    return prev;
+                })
+                .stream()
+                .distinct()
+                .forEach(System.out::print);
     }
 }
