75d00a637825a5d1f8a41e1e30debbf6d8edc189
zhihui.zhou <zhihui.zhou@pcitc.com>
2017-09-05 17:33:50 +0800

    common

---
+++ src/main/java/advance/JVM/InvokeDynamicTest.java
@@ -0,0 +1,46 @@
+package advance.JVM;
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.ConstantCallSite;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+/**
+ * 描述： <br>
+ * 创建时间: 2017/8/3117:40 <br>
+ *
+ * @author 周志辉
+ */
+public class InvokeDynamicTest {
+
+    public static void main(String[] args) throws Throwable {
+        INDY_BootstrapMethod().invokeExact();
+    }
+
+    public static void testMethod(String s) {
+        System.out.println("hello string : " + s);
+    }
+
+    private static MethodHandle INDY_BootstrapMethod() throws Throwable {
+        CallSite cs = (CallSite) MH_BootstrapMethod().invokeWithArguments(MethodHandles.lookup(), "testMethod",
+                MethodType.fromMethodDescriptorString("(Ljava/lang/String;)V", null));
+        return cs.dynamicInvoker();
+    }
+
+
+    private static MethodHandle MH_BootstrapMethod() throws NoSuchMethodException, IllegalAccessException {
+        return MethodHandles.lookup().findStatic(InvokeDynamicTest.class, "BootstrapMethod", MT_BootstrapMethod());
+    }
+
+
+    private static MethodType MT_BootstrapMethod() {
+        return MethodType.fromMethodDescriptorString("(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;" +
+                "Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;",null);
+    }
+
+
+    private static CallSite BootstrapMethod(MethodHandles.Lookup lookup, String name, MethodType mt) throws NoSuchMethodException, IllegalAccessException {
+        return new ConstantCallSite(lookup.findStatic(InvokeDynamicTest.class, name, mt));
+    }
+}
