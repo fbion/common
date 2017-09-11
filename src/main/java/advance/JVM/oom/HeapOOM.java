beab05be393c4b29d2a21c3a2215cd5251badf82
zhihui.zhou <zhihui.zhou@pcitc.com>
2017-09-11 10:57:49 +0800

    common

--- src/main/java/advance/JVM/oom/HeapOOM.java
+++ src/main/java/advance/JVM/oom/HeapOOM.java
@@ -1,13 +1,14 @@
 package advance.JVM.oom;
 
-import com.sun.xml.internal.messaging.saaj.packaging.mime.util.LineInputStream;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Administrator on 2016/5/30.
  * VM Args:-Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+ *
+ * -XX:+HeapDumpOnOutOfMemoryError  JVM发生OOM异常自动生成dummp文件
+ * -XX:+HeapDumpOnCtrlBreak         Ctrl+Break组合键让JVM生成dump文件
  */
 public class HeapOOM {
     static class OOMObject{
