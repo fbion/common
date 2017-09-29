532eb850cdd6f5ba1a29aaf50b445fad3f14e6b4
zhihui.zhou <zhihui.zhou@pcitc.com>
2017-09-29 16:40:22 +0800

    common

--- src/main/java/advance/JVM/concurrent/Test.java
+++ src/main/java/advance/JVM/concurrent/Test.java
@@ -1,44 +1,41 @@
 package advance.JVM.concurrent;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.Vector;
 
 /**
- * 描述： <br>
+ * 描述： 线程安全类Vector出现线程不安全的情况<br>
  * 创建时间: 2017/9/2518:07 <br>
  *
  * @author 周志辉
  */
 public class Test {
-    static int i = 10;
-    static boolean flag = true;
-    public static void main(String[] args) throws InterruptedException {
-        ExecutorService ec = Executors.newCachedThreadPool();
-        ec.submit(() -> {
-            try {
-                flag = true;
-                TimeUnit.SECONDS.sleep(10);
-                i = 11;
-                flag =false;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+    private static Vector<Integer> vector = new Vector<Integer>();
+
+
+    public static void main(String[] args) {
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                vector.add(i);
             }
-            System.out.println("over1");
-        });
-        ec.submit(() -> {
-            while(flag) {
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            Thread removeThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        vector.remove(i);
+                    }
                 }
             }
-            System.out.println(i);
-            System.out.println("over2");
-        });
-        TimeUnit.SECONDS.sleep(12);
-        ec.shutdownNow();
-        System.out.println("main over");
+            );
+            Thread printThread = new Thread(() -> {
+                for (int i = 0; i < vector.size(); i++) {
+                    System.out.println((vector.get(i)));
+                }
+            });
+            removeThread.start();
+            printThread.start();
+            //不要同时产生过多的线程，否则会导致操作系统假死
+            while (Thread.activeCount() > 40) ;
+        }
     }
 }
