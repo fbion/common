1fb969d2351fdfc23156b2ff182853ba476f09cd
周志辉 <zhihui.zhou@pcitc.com>
2017-12-28 11:39:38 +0800

    common

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,3 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 /**
  * description： <br>
  * createTime: 2017/12/717:49 <br>
@@ -15,25 +7,26 @@ import java.net.URL;
 public class Test {
 
     public static void main(String[] args) {
-        if(args.length < 1) {
-            return;
-        }
-        String urlStr = args[0];
-        try(BufferedReader bufr = new BufferedReader(new InputStreamReader(new BufferedInputStream(new URL(urlStr)
-                .openStream()), "utf-8"));) {
-            String line;
-            StringBuffer sb = new StringBuffer();
-            while ((line = bufr.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            bufr.close();
-            System.out.println(sb.toString());
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        if(args.length < 1) {
+//            return;
+//        }
+//        String urlStr = args[0];
+//        try(BufferedReader bufr = new BufferedReader(new InputStreamReader(new BufferedInputStream(new URL(urlStr)
+//                .openStream()), "utf-8"));) {
+//            String line;
+//            StringBuffer sb = new StringBuffer();
+//            while ((line = bufr.readLine()) != null) {
+//                sb.append(line + "\n");
+//            }
+//            bufr.close();
+//            System.out.println(sb.toString());
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("<scriptasjdflksadf".replaceAll("<script", ""));
     }
 }
